0bdbdf64deabd3d5dfcdbc0cea23ab59733fbbe5
HasieEst <ayrtonm0@gmail.com>
2022-10-13 12:50:59 +0300

    Refactoring

--- app.js
+++ app.js
@@ -6,18 +6,12 @@ const form = document.querySelector('form')
 form.addEventListener('submit', addBook)
 
 function addBook(e){
-    // input values
+    // input values.
     const author = authorInput.value
     const book = bookInput.value
     const isbn = isbnInput.value
-    // creating <li> elements
-    const li = document.createElement('li')
-    // defining <li> CSS class
-    li.className = 'list-group-item'
-    const textBook = document.createTextNode(book)
-    const textAuthor = document.createTextNode(isbn)
-    const textIsbn = document.createTextNode(isbn)
-
-
+    console.log(author)
+    console.log(book)
+    console.log(isbn)
     e.preventDefault()
 }
